240bd4861935fba245c8422a1cd26c68b866c9e3
Matt Kline <slavik262@gmail.com>
2011-11-28 10:12:41 -0600

    Fixed CRC code.

--- C/crc32.c
+++ C/crc32.c
@@ -11,12 +11,12 @@ static inline uint32_t Reflect(uint32_t ref, char ch)
 {
 	uint32_t value = 0;
 
-	// Swap bit 0 for bit 7
-	// bit 1 for bit 6, etc.
-	for (int c = 0; c < (ch + 1); ++c)
+	// Swap bit 0 for bit 7, bit 1 for bit 6, etc.
+	for (int pos = 1; pos < ch + 1; ++pos)
 	{
 		if (ref & 1)
-			value |= 1 << (ch - c);
+			value |= 1 << (ch - pos);
+
 		ref >>= 1;
 	}
 
@@ -26,12 +26,13 @@ static inline uint32_t Reflect(uint32_t ref, char ch)
 void CRC32Init()
 {
 	// 256 values representing ASCII character codes. 
-    for(int c = 0; c <= 0xFF; ++c) 
+    for (int c = 0; c <= 0xFF; ++c) 
     { 
 		table[c] = Reflect(c, 8) << 24; 
-        for (int i = 0; i < 8; ++i) 
+        for (int p = 0; p < 8; ++p) 
 		{
-			table[c] = (table[c] << 1) ^ (table[c] & (1 << 31) ? IEEEPoly : 0); 
+			table[c] = (table[c] << 1)
+				^ ((table[c] & (1 << 31)) ? IEEEPoly : 0); 
 		}
         table[c] = Reflect(table[c], 32); 
 	} 
@@ -39,12 +40,11 @@ void CRC32Init()
 
 uint32_t CRC32Generate(void* data, size_t dataLength)
 {
-	register uint32_t crc = 0xFFFFFFFF;
-	char* cData = (char*)data;
+	uint32_t crc = 0xFFFFFFFF;
 
-    for( ; dataLength; --dataLength, ++data)
+    for (uint8_t* cData = (uint8_t*)data; dataLength; --dataLength, ++cData)
     {
-		crc = (table[((crc) ^ ((unsigned char)*cData)) & 0xff] ^ ((crc) >> 8));
+		crc = table[(crc & 0xFF) ^ *cData] ^ (crc >> 8);
     }
 
 	return ~crc;
